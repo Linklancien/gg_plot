e1d52c0319a59729e3f2057d96600f0254f58c05
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-11-29 19:01:00 +0100

    Update gg_plot.v

--- gg_plot.v
+++ gg_plot.v
@@ -317,8 +317,6 @@ fn (dia Diagram) render_axes(ctx gg.Context, min_x f32, max_x f32, min_y f32, ma
 }
 
 fn (dia Diagram) render_labels(ctx gg.Context, min_x f32, max_x f32, min_y f32, max_y f32) {
-	x_title := int((min_x + max_x) / 2)
-	y_title := int(min_y - dia.border / 2)
-	ctx.draw_text(x_title, y_title, 
-	dia.title.text, dia.title.cfg)
+x_title := int((min_x + max_x) / 2)
+y_title := int(min_y - dia.border / 2)ctx.draw_text(x_title, y_title, dia.title.text, dia.title.cfg)
 }
